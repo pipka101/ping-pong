89104c3d3e81d15e299f2ad87cfed3b6d891b146
pipka101 <133512245+pipka101@users.noreply.github.com>
2023-05-18 19:57:44 +0300

    Update ping-pong.py
    запрограммировали перемещение ракеток

--- ping-pong.py
+++ ping-pong.py
@@ -18,13 +18,14 @@ speed = 10
 game = True 
 finich = False
 class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y, player_speed, size_x, size_y):
+    def __init__(self, player_image, x, y, speed, size_x, size_y):
         super().__init__()
         self.image = transform.scale(image.load(player_image), (size_x, size_y))
-        self.speed = player_speed
+        self.speed = speed
         self.rect = self.image.get_rect()
-        self.rect.x = player_x
-        self.rect.y = player_y
+        self.rect.x= x
+        self.rect.y = y
+      
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
@@ -54,10 +55,16 @@ ball = sprite.Group()
 
 class Player(GameSprite):
     def update(self):
-        if keys_pressed[K_LEFT] and self.rect.x > 5:
-            self.rect.x -= speed
-        if keys_pressed[K_RIGHT] and self.rect.x < 595:
-            self.rect.x += speed
+        if keys_pressed[K_UP] :
+            self.rect.y -= speed
+        if keys_pressed[K_DOWN] :
+            self.rect.y += speed
+
+    def update2(self):        
+        if keys_pressed[K_w] :
+            self.rect.y -= speed
+        if keys_pressed[K_s] :
+            self.rect.y += speed
     def fire(self):
         bullet = Bullet('bullet.png', self.rect.centerx, self.rect.top, -15, 15, 20)
         bullets.add(bullet)
@@ -70,20 +77,23 @@ class Enemy(GameSprite):
            self.rect.y = 0
            self.rect.x = randint(0, 600)
            
-x1 = 100
-y1 = 350
-x2 = 200
-y2 = 350
-speed = 10
+x1 = 20
+y1 = 200
+x2 = 650
+y2 = 200
+x3 = 350
+y3 = 200 
+speed = 8
+speed1 = 10
 game = True 
 
 
 
 
 
-player1 = Player('platform.png', player_x, player_y, player_speed, 65, 65 )
-player2 = Player('platform.png', 80, player1_y, randint(2, 3), 50, 50)
-ball = GameSprite('мячик1.png', randint(80, 600), player3_y, randint(1, 2), 50, 50)
+player1 = Player('platform.png', x1, y1, speed, 30, 100 )
+player2 = Player('platform.png', x2, y2, speed, 30, 100)
+ball = GameSprite('мячик1.png', x3, y3, speed1, 120, 80)
     
 win1 = 0
 
@@ -93,51 +103,28 @@ while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-        elif e.type == KEYDOWN:
-            if e.key == K_w:
-                player.fire()
-
+       
     if finich != True:
         window.blit(background, (0,0))
         keys_pressed = key.get_pressed()
+        player1.update()
+        player2.update2()
         player1.reset()
         player2.reset()
         
         ball.reset()
         ball.update()
-        player1.update()
-        player2.update()
+
        
-        text_losts = font2.render('Пропущено: ' + str(lost), 1, (255, 255, 255))
-        window.blit(text_losts, (10, 20))   
-        win2 = font2.render('Счет: ' + str(win1), 1, (255, 255, 255))
-        window.blit(win2, (10, 50))
+      
 
 
     for e in event.get():
         if e.type == QUIT:
             game = False
     keys_pressed = key.get_pressed()
-
-    if keys_pressed[K_LEFT] and x1 > 5:
-        x1 -= speed
-    if keys_pressed[K_RIGHT] and x1 < 595:
-        x1 += speed
-    if keys_pressed[K_UP] and y1 > 5:
-        y1 -= speed
-    if keys_pressed[K_DOWN] and y1 < 395:
-        y1 += speed
-    if keys_pressed[K_a] and x2 > 5:
-        x2 -= speed
-    if keys_pressed[K_d] and x2 < 595:
-        x2 += speed
-    if keys_pressed[K_w] and y2 > 5:
-        y2 -= speed
-    if keys_pressed[K_s] and y2 < 395:
-        y2 += speed
+  
 
 
     display.update()
     clock.tick(60)
-
-
